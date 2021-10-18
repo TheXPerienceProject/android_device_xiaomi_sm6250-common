eed20685156c99ea03656aef2ce0d4c29a401c9f
klozz <klozz@thexperienceproject.org>
2021-10-18 07:52:33 +0200

    sm6250-common: Track qcom sepolicy
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,12 +206,12 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-07-01
 
 # Sepolicy
-#-include device/qcom/sepolicy_vndr/SEPolicy.mk
+-include device/qcom/sepolicy_vndr/SEPolicy.mk
 #SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 #SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 #BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy-minimal
+#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy-minimal
 
 # Treble
 BOARD_VNDK_VERSION := current
