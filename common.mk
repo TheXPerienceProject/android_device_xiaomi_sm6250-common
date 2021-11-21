e6316e30374212f6961f73839da85cb101289110
klozz <klozz@thexperienceproject.org>
2021-11-21 12:10:54 +0100

    sm6250-common: Remove telephony packages
    * They are being builded from our vendor , so , to remove duplication this change
      is needed: follow https://github.com/TheXPerienceProject/android_vendor_xperience/commit/cbc6610f2b2d87fe1d868e472bf4b48c72e410ec
    
    Test: Build and check telephony is working and no crashes occurred.
    
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- common.mk
+++ common.mk
@@ -403,25 +403,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
-    ims-ext-common \
-    ims_ext_common.xml \
     libshim_imsvidecoder \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-hidl-wrapper-prd \
-    qti_telephony_hidl_wrapper_prd.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    tcmiface \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    tcmiface \
-    telephony-ext
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
